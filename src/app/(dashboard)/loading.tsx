4f6666b16bb95707c2ced5e2c83631be06816305
whynotBb <whynot.byoul@gmail.com>
2026-06-23 13:13:03 +0900

    🔥 chore: 스타터킷 보일러플레이트 제거
    - (auth)/(dashboard)/(landing) 구 라우트 그룹 삭제
    - header.tsx, footer.tsx 랜딩 레이아웃 컴포넌트 삭제
    - settings.ts 예제 유효성 스키마 삭제

--- src/app/(dashboard)/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingSpinner } from '@/components/composite/loading-spinner'
-
-export default function DashboardLoading() {
-  return (
-    <div className="flex flex-1 items-center justify-center p-8">
-      <LoadingSpinner size="lg" />
-    </div>
-  )
-}
